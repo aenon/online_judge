896d6bddbd1d9f060dcb48894a4e4c90483b3ec1
Xilin Sun <s.sn.giraffe@gmail.com>
2017-05-30 19:01:13 -0700

    update 182

--- leetcode/186.reverse_words_in_a_string_ii.java
+++ leetcode/186.reverse_words_in_a_string_ii.java
@@ -3,7 +3,19 @@
  */
 
 public class Solution {
+
   public void reverseWords(char[] s) {
-
-
+    int j = s.length();
+    for (int i = s.length() - 1; i >= 0; i--) {
+      if (s.charAt(i) == ' ') {
+        j = i;
+      }
+      else if (i == 0 || s.charAt(i - 1) == ' ') {
+        if (reversed.length() > 0) {
+          reversed.append(' ');
+        }
+        reversed.append(s.substring(i, j));
+      }
+    }
+  }
 }
