002f7861cdaef1d3f33cb96c8aa592c33cf6214e
Xilin Sun <s.sn.giraffe@gmail.com>
2017-05-31 13:46:58 -0700

    add 186

--- leetcode/186.reverse_words_in_a_string_ii.java
+++ leetcode/186.reverse_words_in_a_string_ii.java
@@ -3,19 +3,23 @@
  */
 
 public class Solution {
-
   public void reverseWords(char[] s) {
-    int j = s.length();
-    for (int i = s.length() - 1; i >= 0; i--) {
-      if (s.charAt(i) == ' ') {
-        j = i;
-      }
-      else if (i == 0 || s.charAt(i - 1) == ' ') {
-        if (reversed.length() > 0) {
-          reversed.append(' ');
+    if (s.length > 1) {
+      reverse(s, 0, s.length); // reverse the whole string
+      for (int i = 0, j = 0; j <= s.length; j++) {
+        if (j == s.length || s[j] == ' ') {
+          reverse(s, i, j); // reverse the first word 
+          i = j + 1;
         }
-        reversed.append(s.substring(i, j));
       }
+    }
+  }
+
+  private void reverse(char[] s, int begin, int end) {
+    for (int i = 0; i < (end - begin) / 2; i++) {
+      char temp = s[begin + i];
+      s[begin + i] = s[end - i - 1];
+      s[end - i - 1] = temp;
     }
   }
 }
