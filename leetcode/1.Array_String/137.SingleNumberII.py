77c203a6e9f04f28f7e026d99986a3484ccaae76
Xilin Sun <s.sn.giraffe@gmail.com>
2017-08-18 16:20:54 -0700

    add 136 and 137

---
+++ leetcode/1.Array_String/137.SingleNumberII.py
@@ -0,0 +1,18 @@
+#  137. Single Number II 
+#   Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+# Note:
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? 
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # not a bit manipulation answer 
+
+        return (3*sum(set(nums)) - sum(nums)) / 2
+
+
