b2caff6871d2fcf4e65f61e52a97a04bfa564c2c
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-02 13:23:28 -0700

    add 15.3Sum.py: n^2 method using two pointers

--- leetcode/1.Array_String/15.3Sum.py
+++ leetcode/1.Array_String/15.3Sum.py
@@ -6,7 +6,36 @@
 
 # A solution set is:
 # [
-#   [-1, 0, 1],
-#   [-1, -1, 2]
+#     [-1, 0, 1],
+#     [-1, -1, 2]
 # ]
 
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        Sort the list; for each unique number, find two other numbers with 
+        two-pointers method.
+        """
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            if i > 0 and nums[i] == nums[i - 1]:
+                i += 1
+                continue
+            j, k = i + 1, len(nums) -1
+            while j < k:
+                sum_ijk = nums[i] + nums[j] + nums[k]
+                if sum_ijk == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif sum_ijk < 0:
+                    j += 1
+                else:
+                    k -= 1
+            i += 1
+        return result
